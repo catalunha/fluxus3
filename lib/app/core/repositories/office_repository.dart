1e63731c1d323280935035b9cba554bc04392e84
catalunha <catalunha.mj@gmail.com>
2023-06-10 17:59:47 -0300

    rev

--- lib/app/core/repositories/office_repository.dart
+++ lib/app/core/repositories/office_repository.dart
@@ -9,10 +9,11 @@ class OfficeRepository {
 
   OfficeRepository();
   Future<List<OfficeModel>> list(
-    QueryBuilder<ParseObject> query,
-    Pagination pagination,
-  ) =>
-      apiB4a.list(query, pagination);
+    QueryBuilder<ParseObject> query, {
+    Pagination? pagination,
+    Map<String, List<String>> cols = const {},
+  }) =>
+      apiB4a.list(query, pagination: pagination, cols: cols);
   Future<String> update(OfficeModel model) => apiB4a.update(model);
   Future<bool> delete(String modelId) => apiB4a.delete(modelId);
 }
