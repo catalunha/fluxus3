4f1ab6d20b4e63497b088c4b5aff4dcec383e2e9
catalunha <catalunha.mj@gmail.com>
2023-07-10 09:11:18 -0300

    Buscar Event's pelo profissional Fixes #10

--- lib/app/feature/attendance/search/controller/providers.dart
+++ lib/app/feature/attendance/search/controller/providers.dart
@@ -20,6 +20,14 @@ FutureOr<List<AttendanceModel>> attendanceList(AttendanceListRef ref) async {
   final QueryBuilder<ParseObject> query =
       QueryBuilder<ParseObject>(ParseObject(AttendanceEntity.className));
 
+  if (ref.read(authorizationCodeSelectProvider)) {
+    query.whereEqualTo(AttendanceEntity.authorizationCode,
+        ref.read(authorizationCodeSelectedProvider));
+  }
+  if (ref.read(attendanceIdCodeSelectProvider)) {
+    query.whereEqualTo(
+        AttendanceEntity.id, ref.read(attendanceIdCodeSelectedProvider));
+  }
   if (ref.read(statusSelectProvider)) {
     query.whereEqualTo(
         AttendanceEntity.status,
@@ -48,16 +56,30 @@ FutureOr<List<AttendanceModel>> attendanceList(AttendanceListRef ref) async {
               ..objectId = ref.read(patientSelectedProvider)!.id)
             .toPointer());
   }
-  final start = ref.read(startSearchProvider);
-  final end = ref.read(endSearchProvider);
-  query.whereGreaterThanOrEqualsTo(AttendanceEntity.authorizationDateCreated,
-      DateTime(start.year, start.month, start.day));
-  query.whereLessThanOrEqualTo(AttendanceEntity.authorizationDateCreated,
-      DateTime(end.year, end.month, end.day, 23, 59));
+  if (ref.read(dateSelectProvider)) {
+    final start = ref.read(startSearchProvider);
+    final end = ref.read(endSearchProvider);
+    query.whereGreaterThanOrEqualsTo(AttendanceEntity.authorizationDateCreated,
+        DateTime(start.year, start.month, start.day));
+    query.whereLessThanOrEqualTo(AttendanceEntity.authorizationDateCreated,
+        DateTime(end.year, end.month, end.day, 23, 59));
+  }
   query.orderByDescending('updatedAt');
   return await ref.read(attendanceRepositoryProvider).list(
         query,
       );
+}
+
+@riverpod
+class DateSelect extends _$DateSelect {
+  @override
+  bool build() {
+    return false;
+  }
+
+  void set(bool value) {
+    state = value;
+  }
 }
 
 @riverpod
@@ -80,6 +102,54 @@ class EndSearch extends _$EndSearch {
   }
 
   void set(DateTime value) {
+    state = value;
+  }
+}
+
+@riverpod
+class AuthorizationCodeSelect extends _$AuthorizationCodeSelect {
+  @override
+  bool build() {
+    return false;
+  }
+
+  void set(bool value) {
+    state = value;
+  }
+}
+
+@riverpod
+class AuthorizationCodeSelected extends _$AuthorizationCodeSelected {
+  @override
+  String? build() {
+    return null;
+  }
+
+  void set(String? value) {
+    state = value;
+  }
+}
+
+@riverpod
+class AttendanceIdCodeSelect extends _$AttendanceIdCodeSelect {
+  @override
+  bool build() {
+    return false;
+  }
+
+  void set(bool value) {
+    state = value;
+  }
+}
+
+@riverpod
+class AttendanceIdCodeSelected extends _$AttendanceIdCodeSelected {
+  @override
+  String? build() {
+    return null;
+  }
+
+  void set(String? value) {
     state = value;
   }
 }
