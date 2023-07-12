a2538c0e16efad47c8c15af2ded541e23d09d2cd
catalunha <catalunha.mj@gmail.com>
2023-07-12 12:03:06 -0300

    rev

--- lib/app/feature/home/home_page.dart
+++ lib/app/feature/home/home_page.dart
@@ -30,247 +30,252 @@ class HomePage extends ConsumerWidget with Loader, Messages {
       ),
       body: Center(
         child: SingleChildScrollView(
-          child: Wrap(
+          child: Column(
             children: [
-              HomeModule(
-                access: const ['sec'],
-                title: 'Pacientes',
-                icon: Icons.personal_injury,
-                color: Colors.black87,
-                actions: [
-                  IconButton(
-                    onPressed: () {
-                      context.goNamed(AppPage.patientList.name);
-                    },
-                    icon: const Icon(Icons.list),
+              const Text('Fluxus versão: 3.0.0'),
+              Wrap(
+                children: [
+                  HomeModule(
+                    access: const ['sec'],
+                    title: 'Pacientes',
+                    icon: Icons.personal_injury,
+                    color: Colors.black87,
+                    actions: [
+                      IconButton(
+                        onPressed: () {
+                          context.goNamed(AppPage.patientList.name);
+                        },
+                        icon: const Icon(Icons.list),
+                      ),
+                      // IconButton(
+                      //   onPressed: () {
+                      //     context.goNamed(AppPage.patientList.name);
+                      //   },
+                      //   icon: const Icon(Icons.search),
+                      // )
+                    ],
                   ),
-                  // IconButton(
-                  //   onPressed: () {
-                  //     context.goNamed(AppPage.patientList.name);
-                  //   },
-                  //   icon: const Icon(Icons.search),
-                  // )
-                ],
-              ),
-              HomeModule(
-                access: const ['sec'],
-                title: 'Atendimentos',
-                icon: Icons.book,
-                color: Colors.black87,
-                actions: [
-                  IconButton(
-                    onPressed: () {
-                      context.goNamed(AppPage.attendanceAdd.name);
-                    },
-                    icon: const Icon(Icons.add),
+                  HomeModule(
+                    access: const ['sec'],
+                    title: 'Atendimentos',
+                    icon: Icons.book,
+                    color: Colors.black87,
+                    actions: [
+                      IconButton(
+                        onPressed: () {
+                          context.goNamed(AppPage.attendanceAdd.name);
+                        },
+                        icon: const Icon(Icons.add),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          context.goNamed(AppPage.attendanceSearch.name);
+                        },
+                        icon: const Icon(Icons.search),
+                      )
+                    ],
                   ),
-                  IconButton(
-                    onPressed: () {
-                      context.goNamed(AppPage.attendanceSearch.name);
-                    },
-                    icon: const Icon(Icons.search),
-                  )
-                ],
-              ),
-              HomeModule(
-                access: const ['sec'],
-                title: 'Eventos',
-                icon: Icons.maps_home_work_rounded,
-                color: Colors.black87,
-                actions: [
-                  IconButton(
-                    onPressed: () {
-                      context.goNamed(AppPage.eventSave.name);
-                    },
-                    icon: const Icon(Icons.add),
+                  HomeModule(
+                    access: const ['sec'],
+                    title: 'Eventos',
+                    icon: Icons.maps_home_work_rounded,
+                    color: Colors.black87,
+                    actions: [
+                      IconButton(
+                        onPressed: () {
+                          context.goNamed(AppPage.eventSave.name);
+                        },
+                        icon: const Icon(Icons.add),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          context.goNamed(AppPage.eventSearch.name);
+                        },
+                        icon: const Icon(Icons.search),
+                      )
+                    ],
                   ),
-                  IconButton(
-                    onPressed: () {
-                      context.goNamed(AppPage.eventSearch.name);
-                    },
-                    icon: const Icon(Icons.search),
-                  )
-                ],
-              ),
-              HomeModule(
-                access: const ['sec'],
-                title: 'Agenda',
-                icon: Icons.calendar_month,
-                color: Colors.black87,
-                actions: [
-                  IconButton(
-                    onPressed: () {
-                      context.goNamed(AppPage.schedule.name);
-                    },
-                    icon: const Icon(Icons.view_timeline_sharp),
+                  HomeModule(
+                    access: const ['sec'],
+                    title: 'Agenda',
+                    icon: Icons.calendar_month,
+                    color: Colors.black87,
+                    actions: [
+                      IconButton(
+                        onPressed: () {
+                          context.goNamed(AppPage.schedule.name);
+                        },
+                        icon: const Icon(Icons.view_timeline_sharp),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              HomeModule(
-                access: const ['admin', 'sec', 'prof'],
-                title: 'Prontuário',
-                icon: Icons.folder_shared_outlined,
-                color: Colors.black,
-                actions: [
-                  IconButton(
-                    onPressed: () {
-                      context.goNamed(AppPage.sharedPatientList.name);
-                    },
-                    icon: const Icon(Icons.list),
+                  HomeModule(
+                    access: const ['admin', 'sec', 'prof'],
+                    title: 'Prontuário',
+                    icon: Icons.folder_shared_outlined,
+                    color: Colors.black,
+                    actions: [
+                      IconButton(
+                        onPressed: () {
+                          context.goNamed(AppPage.sharedPatientList.name);
+                        },
+                        icon: const Icon(Icons.list),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              HomeModule(
-                access: const ['admin'],
-                title: 'Gerenciar Usuários',
-                icon: Icons.people,
-                color: Colors.black,
-                actions: [
-                  IconButton(
-                    onPressed: () {
-                      context.goNamed(AppPage.userProfileList.name);
-                    },
-                    icon: const Icon(Icons.list),
+                  HomeModule(
+                    access: const ['admin'],
+                    title: 'Gerenciar Usuários',
+                    icon: Icons.people,
+                    color: Colors.black,
+                    actions: [
+                      IconButton(
+                        onPressed: () {
+                          context.goNamed(AppPage.userProfileList.name);
+                        },
+                        icon: const Icon(Icons.list),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              HomeModule(
-                access: const ['admin'],
-                title: 'Cargos',
-                icon: Icons.power_input_sharp,
-                color: Colors.black,
-                actions: [
-                  IconButton(
-                    onPressed: () {
-                      context.goNamed(AppPage.officeList.name);
-                    },
-                    icon: const Icon(Icons.list),
+                  HomeModule(
+                    access: const ['admin'],
+                    title: 'Cargos',
+                    icon: Icons.power_input_sharp,
+                    color: Colors.black,
+                    actions: [
+                      IconButton(
+                        onPressed: () {
+                          context.goNamed(AppPage.officeList.name);
+                        },
+                        icon: const Icon(Icons.list),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              HomeModule(
-                access: const ['admin'],
-                title: 'Procedimentos',
-                icon: Icons.construction_sharp,
-                color: Colors.black,
-                actions: [
-                  IconButton(
-                    onPressed: () {
-                      context.goNamed(AppPage.procedureList.name);
-                    },
-                    icon: const Icon(Icons.list),
+                  HomeModule(
+                    access: const ['admin'],
+                    title: 'Procedimentos',
+                    icon: Icons.construction_sharp,
+                    color: Colors.black,
+                    actions: [
+                      IconButton(
+                        onPressed: () {
+                          context.goNamed(AppPage.procedureList.name);
+                        },
+                        icon: const Icon(Icons.list),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              HomeModule(
-                access: const ['admin'],
-                title: 'Especialidades',
-                icon: Icons.folder_special,
-                color: Colors.black,
-                actions: [
-                  IconButton(
-                    onPressed: () {
-                      context.goNamed(AppPage.expertiseList.name);
-                    },
-                    icon: const Icon(Icons.list),
+                  HomeModule(
+                    access: const ['admin'],
+                    title: 'Especialidades',
+                    icon: Icons.folder_special,
+                    color: Colors.black,
+                    actions: [
+                      IconButton(
+                        onPressed: () {
+                          context.goNamed(AppPage.expertiseList.name);
+                        },
+                        icon: const Icon(Icons.list),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              HomeModule(
-                access: const ['sec'],
-                title: 'Região',
-                icon: Icons.bubble_chart_outlined,
-                color: Colors.black,
-                actions: [
-                  IconButton(
-                    onPressed: () {
-                      context.goNamed(AppPage.regionList.name);
-                    },
-                    icon: const Icon(Icons.list),
+                  HomeModule(
+                    access: const ['sec'],
+                    title: 'Região',
+                    icon: Icons.bubble_chart_outlined,
+                    color: Colors.black,
+                    actions: [
+                      IconButton(
+                        onPressed: () {
+                          context.goNamed(AppPage.regionList.name);
+                        },
+                        icon: const Icon(Icons.list),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              HomeModule(
-                access: const ['admin'],
-                title: 'Sala',
-                icon: Icons.house_sharp,
-                color: Colors.black,
-                actions: [
-                  IconButton(
-                    onPressed: () {
-                      context.goNamed(AppPage.roomList.name);
-                    },
-                    icon: const Icon(Icons.list),
+                  HomeModule(
+                    access: const ['admin'],
+                    title: 'Sala',
+                    icon: Icons.house_sharp,
+                    color: Colors.black,
+                    actions: [
+                      IconButton(
+                        onPressed: () {
+                          context.goNamed(AppPage.roomList.name);
+                        },
+                        icon: const Icon(Icons.list),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              HomeModule(
-                access: const ['admin'],
-                title: 'Status',
-                icon: Icons.start,
-                color: Colors.black,
-                actions: [
-                  IconButton(
-                    onPressed: () {
-                      context.goNamed(AppPage.statusList.name);
-                    },
-                    icon: const Icon(Icons.list),
+                  HomeModule(
+                    access: const ['admin'],
+                    title: 'Status',
+                    icon: Icons.start,
+                    color: Colors.black,
+                    actions: [
+                      IconButton(
+                        onPressed: () {
+                          context.goNamed(AppPage.statusList.name);
+                        },
+                        icon: const Icon(Icons.list),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              HomeModule(
-                access: const ['admin'],
-                title: 'Tipos de Planos de Saúde',
-                icon: Icons.credit_card_rounded,
-                color: Colors.black,
-                actions: [
-                  IconButton(
-                    onPressed: () {
-                      context.goNamed(AppPage.healthPlanTypeList.name);
-                    },
-                    icon: const Icon(Icons.list),
+                  HomeModule(
+                    access: const ['admin'],
+                    title: 'Tipos de Planos de Saúde',
+                    icon: Icons.credit_card_rounded,
+                    color: Colors.black,
+                    actions: [
+                      IconButton(
+                        onPressed: () {
+                          context.goNamed(AppPage.healthPlanTypeList.name);
+                        },
+                        icon: const Icon(Icons.list),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              HomeModule(
-                access: const ['admin'],
-                title: 'Anamnese - Grupos',
-                icon: Icons.question_answer,
-                color: Colors.black,
-                actions: [
-                  IconButton(
-                    onPressed: () {
-                      context.goNamed(AppPage.anamneseGroupList.name);
-                    },
-                    icon: const Icon(Icons.list),
+                  HomeModule(
+                    access: const ['admin'],
+                    title: 'Anamnese - Grupos',
+                    icon: Icons.question_answer,
+                    color: Colors.black,
+                    actions: [
+                      IconButton(
+                        onPressed: () {
+                          context.goNamed(AppPage.anamneseGroupList.name);
+                        },
+                        icon: const Icon(Icons.list),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              HomeModule(
-                access: const ['admin'],
-                title: 'Anamnese - Perguntas',
-                icon: Icons.question_mark,
-                color: Colors.black,
-                actions: [
-                  IconButton(
-                    onPressed: () {
-                      context.goNamed(AppPage.anamneseQuestionList.name);
-                    },
-                    icon: const Icon(Icons.list),
+                  HomeModule(
+                    access: const ['admin'],
+                    title: 'Anamnese - Perguntas',
+                    icon: Icons.question_mark,
+                    color: Colors.black,
+                    actions: [
+                      IconButton(
+                        onPressed: () {
+                          context.goNamed(AppPage.anamneseQuestionList.name);
+                        },
+                        icon: const Icon(Icons.list),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              HomeModule(
-                access: const ['prof'],
-                title: 'Anamnese - Respostas',
-                icon: Icons.receipt_long,
-                color: Colors.black,
-                actions: [
-                  IconButton(
-                    onPressed: () {
-                      context.goNamed(AppPage.anamnesePeopleList.name);
-                    },
-                    icon: const Icon(Icons.list),
+                  HomeModule(
+                    access: const ['prof'],
+                    title: 'Anamnese - Respostas',
+                    icon: Icons.receipt_long,
+                    color: Colors.black,
+                    actions: [
+                      IconButton(
+                        onPressed: () {
+                          context.goNamed(AppPage.anamnesePeopleList.name);
+                        },
+                        icon: const Icon(Icons.list),
+                      ),
+                    ],
                   ),
                 ],
               ),
